c0405d9ddae110530dbf4dbdefcc14264d46786e
F John De Costa <john@mayerfleur.com>
2019-02-23 16:57:00 +1100

    keyboard sounds playing

--- src/App.js
+++ src/App.js
@@ -4,11 +4,40 @@ import "./App.css";
 import {DrumDisplay} from '../src/Components/DrumDisplay'
 
 
-
+let logKey="";
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			drumPads: [
+				{ id: "Q", sound: "Heater1" },
+				{ id: "W", sound: "Heater2" },
+				{ id: "E", sound: "BigRackTom" },
+				{ id: "A", sound: "Crash" },
+				{ id: "S", sound: "FloorTom" },
+				{ id: "D", sound: "HiHatClosed" },
+				{ id: "Z", sound: "Kick" },
+				{ id: "X", sound: "SmallRackTom" },
+				{ id: "C", sound: "Snare" }
+			],
+			playing: 'Start playing'
+		};
+	}
+	playSound = instrument => {
+		instrument.currentTime = 0;
+		instrument.play();
+		this.state.drumPads.map((e) => {
+			if (e.id === instrument.id) {
+				this.setState({ playing: e.sound })
+			}
+		})
+	};
   render() {
-		
+		logKey = e => {
+			this.playSound(document.getElementById(`${e.key.toUpperCase()}`));
+		};
+		document.querySelector("html").onkeydown = logKey;
     return (
 			<React.Fragment>
 				
--- src/Components/DrumDisplay.js
+++ src/Components/DrumDisplay.js
@@ -1,132 +1,151 @@
 import React from "react";
 
 export function DrumDisplay() {
-  const Q = document.getElementById("Q");
+  
   return (
     <React.Fragment>
       <div id="drum-machine">
-        <div id="display">hello from display</div>
+        <div id="display">
+Click to Play
+				</div>
 
 				{/*Button 'Q' on Drum Pad */}
-				<div className="drum-pad" id="heater1">
-        	<audio className="clip" id="Q" name="Heater 1">
-            <source src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" />
-          </audio>
+				
+        	
 
 					{
-						<button 
-							onClick={() => document.getElementById("Q").play()}>
-							<p>Q</p>
+						<button className="drum-pad" id="heater1"
+							onClick={() => document.getElementById("Q").play()}
+							onMouseUp={() => document.getElementById("display").innerHTML="Heater 1"}
+						>
+						<audio className="clip" id="Q" 
+							 src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" />
+						
+							Q
 						</button>
 					}
 				
-        </div>
+        
 	
 				{/*Button 'W' on Drum Pad */}
-<div>
-
-				<audio className="clip" id="W" name="Heater 2">
-					<source src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" />
-				</audio>
 
 				{
 					<button className="drum-pad" id="heater2"
-						onClick={() => document.getElementById("W").play()}>
-						<p>W</p>
+						onClick={() => document.getElementById("W").play()}
+							onMouseUp={() => document.getElementById("display").innerHTML = "Heater 2"}>
+
+						<audio className="clip" id="W" 
+						src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" />
+						
+
+						W
 					</button>
 				}
-</div>
-				{/*Button 'E' on Drum Pad */}
-				<div>
-				<audio className="clip" id="E" name="bigRackTom">
-					<source src="https://tympanus.net/Tutorials/SVGDrums/mp3/Big-Rack-Tom.mp3" />
-				</audio>
 
+				{/*Button 'E' on Drum Pad */}
+			
 				{
 					<button className="drum-pad" id="bigRackTom"
-						onClick={() => document.getElementById("E").play()}>
-						<p>E</p>
+						onClick={() => document.getElementById("E").play()}
+							onMouseUp={() => document.getElementById("display").innerHTML = "Big Rack Tom"}>
+
+						<audio className="clip" id="E"
+						 src="https://tympanus.net/Tutorials/SVGDrums/mp3/Big-Rack-Tom.mp3" />
+						
+
+						E
 					</button>
 				}
-</div>
-				{/*Button 'A' on Drum Pad */}
-				<div>
-				<audio className="clip" id="A" name="Crash">
-					<source src="https://tympanus.net/Tutorials/SVGDrums/mp3/Crash.mp3" />
-				</audio>
 
+				{/*Button 'A' on Drum Pad */}
+			
 				{
 					<button className="drum-pad" id="Crash"
-						onClick={() => document.getElementById("A").play()}>
-						<p>A</p>
+						onClick={() => document.getElementById("A").play()}
+							onMouseUp={() => document.getElementById("display").innerHTML = "Crash"}>
+
+						<audio className="clip" id="A"
+						src="https://tympanus.net/Tutorials/SVGDrums/mp3/Crash.mp3" />
+						
+
+						A
 					</button>
 				}
-</div>
-				{/*Button 'S' on Drum Pad */}
-				<div>
-				<audio className="clip" id="S" name="Floor Tom">
-					<source src="https://tympanus.net/Tutorials/SVGDrums/mp3/Floor-Tom.mp3" />
-				</audio>
 
+				{/*Button 'S' on Drum Pad */}
+			
 				{
 					<button className="drum-pad" id="floorTom"
-						onClick={() => document.getElementById("S").play()}>
-						<p>S</p>
+						onClick={() => document.getElementById("S").play()}
+							onMouseUp={() => document.getElementById("display").innerHTML = "Floor Tom"}>
+
+						<audio className="clip" id="S" 
+						src="https://tympanus.net/Tutorials/SVGDrums/mp3/Floor-Tom.mp3" />
+						
+
+						S
 					</button>
 				}
-      </div>  
+        
 				{/*Button 'D' on Drum Pad */}
-				<div>
-				<audio className="clip" id="D" name="High Hat Closed">
-					<source src="https://tympanus.net/Tutorials/SVGDrums/mp3/Hi-Hat-Closed.mp3" />
-				</audio>
-
+			
 				{
 					<button className="drum-pad" id="highHatClosed"
-						onClick={() => document.getElementById("D").play()}>
-						<p>D</p>
+						onClick={() => document.getElementById("D").play()}
+							onMouseUp={() => document.getElementById("display").innerHTML = "High Hat Closed"}>
+
+						<audio className="clip" id="D" 
+						 src="https://tympanus.net/Tutorials/SVGDrums/mp3/Hi-Hat-Closed.mp3" />
+						
+
+						D
 					</button>
 				}  
-</div>
-				{/*Button 'Z' on Drum Pad */}
-				<div>
-				<audio className="clip" id="Z" name="Kick">
-					<source src="https://tympanus.net/Tutorials/SVGDrums/mp3/Kick.mp3" />
-				</audio>
 
+				{/*Button 'Z' on Drum Pad */}
+			
 				{
 					<button className="drum-pad" id="kick"
-						onClick={() => document.getElementById("Z").play()}>
-						<p>Z</p>
+						onClick={() => document.getElementById("Z").play()}
+							onMouseUp={() => document.getElementById("display").innerHTML = "Kick"}>
+
+						<audio className="clip" id="Z"  src="https://tympanus.net/Tutorials/SVGDrums/mp3/Kick.mp3" />
+						
+
+						Z
 					</button>
 				}     
-</div>
-				{/*Button 'X' on Drum Pad */}
-				<div>
-				<audio className="clip" id="X" name="Small Rack Tom">
-					<source src="https://tympanus.net/Tutorials/SVGDrums/mp3/Small-Rack-Tom.mp3" />
-				</audio>
 
+				{/*Button 'X' on Drum Pad */}
+			
 				{
 					<button className="drum-pad" id="smallRackTom"
-						onClick={() => document.getElementById("X").play()}>
-						<p>X</p>
+						onClick={() => document.getElementById("X").play()}
+							onMouseUp={() => document.getElementById("display").innerHTML = "Small Rack Tom"}>
+
+						<audio className="clip" id="X" 
+						 src="https://tympanus.net/Tutorials/SVGDrums/mp3/Small-Rack-Tom.mp3" />
+						
+
+						X
 					</button>
 				} 
-</div>
-				{/*Button 'C' on Drum Pad */}
-				<div>
-				<audio className="clip" id="C" name="Snare">
-					<source src="https://tympanus.net/Tutorials/SVGDrums/mp3/Snare.mp3" />
-				</audio>
 
+				{/*Button 'C' on Drum Pad */}
+			
 				{
 					<button className="drum-pad" id="snare"
-						onClick={() => document.getElementById("C").play()}>
-						<p>C</p>
+						onClick={() => document.getElementById("C").play()}
+							onMouseUp={() => document.getElementById("display").innerHTML = "Snare"}>
+
+						<audio className="clip" id="C" 
+						 src="https://tympanus.net/Tutorials/SVGDrums/mp3/Snare.mp3" />
+						
+
+						C
 					</button>
 				} 
-				</div>
+				
       </div>
     </React.Fragment>
   );
